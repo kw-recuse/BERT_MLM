105090788faa7c2c8bf77af7fd3c4ba8ad18fda7
seungjun-green <lsj3285007@gmail.com>
2025-04-29 15:17:34 +0900

    feat: update to support all MLM

--- models/base.py
+++ models/base.py
@@ -1,13 +1,7 @@
 
-from transformers import LongformerTokenizer, LongformerForMaskedLM
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 def load_tokenizer_and_model(model_name):
-    if model_name == "allenai/longformer-base-4096":
-        tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-base-4096") 
-        model = LongformerForMaskedLM.from_pretrained("allenai/longformer-base-4096")
-        return tokenizer, model
-    elif model_name == "something else":
-        tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-base-4096") 
-        model = LongformerForMaskedLM.from_pretrained("allenai/longformer-base-4096")
-    else:
-        raise ValueError("Unsupported model type.")
+    tokenizer = AutoTokenizer.from_pretrained(model_name) 
+    model = AutoModelForMaskedLM.from_pretrained(model_name)
+    return tokenizer, model
